8f9828a8314c304c1a2553ce8fbc4eb3d596468e
alpankaraca <alpann@gmail.com>
2015-06-12 00:06:56 +0300

    Initial Commit

---
+++ SingleViewTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleViewTestApp
+//
+//  Created by Alpan Karaca on 12/06/15.
+//  Copyright (c) 2015 Alpan Karaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
