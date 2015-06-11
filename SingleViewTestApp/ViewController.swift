bd1d491766148fd9b66ee0ce2aa59dc2d7255c09
alpankaraca <alpann@gmail.com>
2015-06-12 01:41:55 +0300

    asd

--- SingleViewTestApp/ViewController.swift
+++ SingleViewTestApp/ViewController.swift
@@ -8,12 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    // Animasyon
+    // animator = UIDynamicAnimator(referenceView: view)
+    var animator: UIDynamicAnimator!
+    
+    // Yer çekimi için
+    // gravity = UIGravityBehavior(items: [square])
+    var gravity: UIGravityBehavior!
+    
+    // Boundaries
+    var collision: UICollisionBehavior!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        square.backgroundColor = UIColor.grayColor()
+        view.addSubview(square)
+        
+        let barrier = UIView(frame: CGRect(x: 0, y: 300, width: 130, height: 20))
+        barrier.backgroundColor = UIColor.redColor()
+        view.addSubview(barrier)
+        
+        // Yer çekimi ve animasyon
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [square])
+        animator.addBehavior(gravity)
+        
+        // Boundaries
+        collision = UICollisionBehavior(items: [square])
+        // add a boundary that has the same frame as the barrier
+        collision.addBoundaryWithIdentifier("barrier", forPath: UIBezierPath(rect: barrier.frame))
+        
+        collision.collisionDelegate = self
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
+        collision.action = {
+            //println("\(NSStringFromCGAffineTransform(square.transform)) \(NSStringFromCGPoint(square.center))")
+        }
+        
     }
+    
+    func collisionBehavior(behavior: UICollisionBehavior!, beganContactForItem item: UIDynamicItem!, withBoundaryIdentifier identifier: NSCopying!, atPoint p: CGPoint) {
+        println("Boundary contact occurred - \(identifier)")
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
